eedd8e3e9e8c868c51be65b41571f8ce6ed57542
Оля Валиева < @DESKTOP-L8ODLH7>
2026-01-11 21:04:38 +0300

    feat(domain): add Role entity with basis properties

---
+++ AuthMicroservice.Domain/Entities/Role.cs
@@ -0,0 +1,28 @@
+﻿namespace AuthMicroservice.Domain.Entities
+{
+    /// <summary>
+    /// Роль в системе
+    /// </summary>
+    public class Role
+    {
+        /// <summary>
+        /// Уникальный идентификатор роли
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Название роли (например, "admin")
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Описание роли
+        /// </summary>
+        public string Description { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Связь: пользователи с этой ролью
+        /// </summary>
+        public ICollection<User> Users { get; set; } = [];
+    }
+}
