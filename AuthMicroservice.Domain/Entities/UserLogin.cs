fbdcd62ed193c9f8244ee7b0285a8b2ba1f77e84
Оля Валиева < @DESKTOP-L8ODLH7>
2026-01-12 08:55:26 +0300

    feat(domain): add UserLogin entity for authentication audit lgging

---
+++ AuthMicroservice.Domain/Entities/UserLogin.cs
@@ -0,0 +1,44 @@
+﻿namespace AuthMicroservice.Domain.Entities
+{
+    /// <summary>
+    /// Запись об аутентификации пользователя
+    /// </summary>
+    public class UserLogin
+    {
+        /// <summary>
+        /// Уникальный идентификатор записи 
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Уникальный идентификатор пользователя 
+        /// Может быть null при неудачном входе
+        /// </summary>
+        public Guid? UserId { get; set; }
+
+        /// <summary>
+        /// Email, по которому пытались войти
+        /// </summary>
+        public string Email { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Успешна ли попытка входа
+        /// </summary>
+        public bool IsSucces { get; set; }
+
+        /// <summary>
+        /// IP-адрес клиента
+        /// </summary>
+        public string? ClientIp { get; set; }
+
+        /// <summary>
+        /// User-Agent
+        /// </summary>
+        public string? UserAgent { get; set; }
+
+        /// <summary>
+        /// Время события
+        /// </summary>
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+    }
+}
